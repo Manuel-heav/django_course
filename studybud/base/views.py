3fe1dd49f27411ff7356107f8ff6035fd60c143b
manuel-heav <manuelheav32@gmail.com>
2022-05-16 20:19:52 +0300

    working on templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+rooms = [
+     {'id': 1, 'name': 'Lets learn python'},
+     {'id': 2, 'name': 'Lets learn design'},
+     {'id': 3, 'name': 'Lets learn sup'},
+]
 # To make a get request function here
 # and this is the same as App.get
 def home(request):
-     return render(request, 'home.html')
+     return render(request, 'home.html',{'rooms': rooms})
 
 def room(request):
      return render(request, 'room.html')
