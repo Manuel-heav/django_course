a22036befef387e64f32c582bfb4df103870356c
manuel-heav <manuelheav32@gmail.com>
2022-05-16 20:47:36 +0300

    working on templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -13,5 +13,10 @@ def home(request):
      return render(request, 'base/home.html',{'rooms': rooms})
 
 def room(request,pk):
-     return render(request, 'base/room.html')
+     room = None
+     for i in rooms:
+          if i['id'] == int(pk):
+               room =i
+     context = {'room': room}
+     return render(request, 'base/room.html', context)
 
