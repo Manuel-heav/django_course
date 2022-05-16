ab2298d4b2114ed9a5d133d38232cc2ae67fc433
manuel-heav <manuelheav32@gmail.com>
2022-05-16 20:12:17 +0300

    working on templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
 
 # To make a get request function here
 # and this is the same as App.get
 def home(request):
-     return HttpResponse('Home page')
+     return render(request, 'home.html')
 
 def room(request):
-     return HttpResponse('Room')
+     return render(request, 'room.html')
+
