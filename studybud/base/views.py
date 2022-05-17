cd81b66a1eb301f74c3cb5a1cba8179d5feaeb7f
manuel-heav <manuelheav32@gmail.com>
2022-05-17 19:48:32 +0300

    working on fetching models

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-     {'id': 1, 'name': 'Lets learn python'},
-     {'id': 2, 'name': 'Lets learn design'},
-     {'id': 3, 'name': 'Lets learn sup'},
-]
+# rooms = [
+#      {'id': 1, 'name': 'Lets learn python'},
+#      {'id': 2, 'name': 'Lets learn design'},
+#      {'id': 3, 'name': 'Lets learn sup'},
+# ]
 # To make a get request function here
 # and this is the same as App.get
 def home(request):
+     rooms = Room.objects.all()
      return render(request, 'base/home.html',{'rooms': rooms})
 
 def room(request,pk):
-     room = None
-     for i in rooms:
-          if i['id'] == int(pk):
-               room =i
+     room = Room.objects.get(id=pk)
      context = {'room': room}
      return render(request, 'base/room.html', context)
 
