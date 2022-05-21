cc2868adf5b65c027a84429a1fee4a6a788d0ad5
manuel-heav <manuelheav32@gmail.com>
2022-05-21 18:45:01 +0300

    working on form

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/', views.createRoom, name="create-room"),
-    path('update-room/<str:pk>/', views.updateRoom, name="update-room")
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room")
 
 ]
