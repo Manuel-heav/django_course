b6edf2279d6b143ef6128e834ea259fa0592fd78
manuel-heav <manuelheav32@gmail.com>
2022-05-27 21:53:17 +0300

    working on user registration

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
