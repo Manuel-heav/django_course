2700dabb3c4bde576aabb81169855517fdcb53f9
manuel-heav <manuelheav32@gmail.com>
2022-05-29 16:47:23 +0300

    working on message CRUd

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -131,3 +131,14 @@ def deleteRoom(request, pk):
           room.delete()
           return redirect('home')
      return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+     message  = Message.objects.get(id=pk)
+     if request.user != message.user:
+          return HttpResponse('your are not allowed')
+
+     if request.method == 'POST':
+          message.delete()
+          return redirect('home')
+     return render(request, 'base/delete.html', {'obj': room})
