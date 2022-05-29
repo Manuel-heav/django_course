7175f738ebbaff59fa0c3f22f16cf70a56b27215
manuel-heav <manuelheav32@gmail.com>
2022-05-29 16:27:54 +0300

    working on user registration

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -80,6 +80,11 @@ def home(request):
 def room(request,pk):
      room = Room.objects.get(id=pk)
      room_messages =  room.message_set.all().order_by('-created')
+
+     if request.method == 'POST':
+          message = Message.objects.create(
+
+          )
      context = {'room': room, 'room_messages': room_messages}
      return render(request, 'base/room.html', context)
 
