f05d928072ff4726e27ddc3a49b5256983cacaee
manuel-heav <manuelheav32@gmail.com>
2022-06-04 19:29:41 +0300

    working on profle page

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -97,9 +97,9 @@ def room(request,pk):
 def userProfile(request,pk):
      user = User.objects.get(id=pk)
      rooms = user.room_set.all()
-     room_message = user.message_set.all()
+     room_messages = user.message_set.all()
      topics = Topic.objects.all()
-     context = {'user': user, 'rooms':rooms, 'room_message': room_message}
+     context = {'user': user, 'rooms':rooms, 'room_messages': room_messages, 'topics': topics}
      return render(request, 'base/profile.html', context)
 
 
